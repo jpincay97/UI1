c5b4e0e6a3e3de5bf70e62dfbd7fe80250053c77
John Pincay <jpincay97@users.noreply.github.com>
2020-06-22 21:12:21 -0500

    Se añadió un diálogo para la fecha

--- app/src/main/java/com/example/ui1/MainActivity.java
+++ app/src/main/java/com/example/ui1/MainActivity.java
@@ -2,21 +2,59 @@ package com.example.ui1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private int rbMasculino;
     private int masculino;
+    private int anio, mes, dia, anioDialog, mesDialog, diaDialog;
+    static final int IdFecha = 0;
+    Calendar C = Calendar.getInstance();
+    EditText txtFechaNac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mesDialog = C.get(Calendar.MONTH);
+        diaDialog = C.get(Calendar.DAY_OF_MONTH);
+        anioDialog = C.get(Calendar.YEAR);
+        txtFechaNac = (EditText) findViewById(R.id.txtFechaNac);
+
+        txtFechaNac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showDialog(IdFecha);
+            }
+        });
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    anio = year; mes = month; dia = day;
+                    txtFechaNac.setText(dia + "/" + (mes + 1) +"/" + anio);
+                }
+            };
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case IdFecha:
+                return new DatePickerDialog(this, mDateSetListener, anioDialog, mesDialog, diaDialog);
+        }
+        return null;
     }
 
     public void btnEnviar(View view) {
@@ -25,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         EditText txtNombre = (EditText) findViewById(R.id.txtNombre);
         RadioButton rbMasculino = (RadioButton) findViewById(R.id.rbMasculino);
         RadioButton rbFemenino = (RadioButton) findViewById(R.id.rbFemenino);
-        EditText txtFechaNac = (EditText) findViewById(R.id.txtFechaNac);
+        //EditText txtFechaNac = (EditText) findViewById(R.id.txtFechaNac);
         EditText txtTelefono = (EditText) findViewById(R.id.txtTelefono);
 
         Bundle b = new Bundle();
